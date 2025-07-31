6247cc0466fda3e475f32534e7e15f748a408f70
bhavya. arora <bhavyaarora@BhavyaArora-PC.local>
2025-07-31 14:21:06 +0530

    fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2144445707"
+    serviceWorkerVersion: "2277975934"
   }
 });
